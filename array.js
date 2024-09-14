785bb7428e263d3b08de2f734946dc4c7b2318b2
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2024-09-14 23:57:56 +0500

    Some changes

---
+++ array.js
@@ -0,0 +1,15 @@
+// Arrays - varaibles that hold multiple values
+const numbers = new Array(1,2,3, 4, 5)
+
+const fruits = ['Apple', 'Orange', "Quince"]
+
+// Adding item to end of an array
+fruits[3] = 'Grapes'
+fruits.push('PineApple')
+
+// Adding item to begining of an array
+fruits.unshift('Pomigranate')
+
+
+console.log(numbers)
+console.log(fruits)
---
+++ dataTypes.js
@@ -0,0 +1,17 @@
+// String, Numbers, Boolean, null, undefined, BigInt, Symbol
+
+const name = 'Asadulloh'
+const age = 21;
+const rating = 4.5 //There is no decimal data types in javaScript
+const isCool = true;
+const x = null // null equals to object it is object type data type
+const c = undefined //Typeof undefined is undefined
+
+
+// Concatenation
+console.log('My name is' + name + ' and I am ' + age + ' years old')
+// Template string
+console.log(`My name is  ${name} and I am  ${age}  years old`)
+
+console.log(name.length)
+console.log(name.split(','))
